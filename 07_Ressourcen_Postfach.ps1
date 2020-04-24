c872a3c07462129428eddbf59ef7f10de9159b55
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-04-24 09:20:04 +0200

    Add files via upload

---
+++ 07_Ressourcen_Postfach.ps1
@@ -0,0 +1,23 @@
+﻿Set-Location c:\
+Clear-Host
+
+$UserCredential = Get-Credential
+
+#Exchange
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session
+
+#Raumpostfach
+New-Mailbox -Name ZMRoom -DisplayName "Zuerich Meeting Raum" -Room
+
+Set-CalendarProcessing ZMRoom -AutomateProcessing AutoAccept
+
+#Gerätepostfach
+New-Mailbox -Name "Mobiler Beamer" -Equipment
+
+Get-Mailbox "ZMRoom" | Select-Object emailaddresses, Identity, Resourcetype
+
+Get-Mailbox "Mobiler Beamer" | Select-Object emailaddresses, Identity, Resourcetype
+
+Remove-PSSession $Session
