f0be4b69dcb45c8674fdc4feb5efe25527a1af31
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-07-31 14:18:49 +0200

    Add files via upload

---
+++ 13_Massenvorgang.ps1
@@ -0,0 +1,20 @@
+﻿Set-Location c:\
+Clear-Host
+
+#Connect to Exchange
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session -AllowClobber
+
+#Sind wir verbunden?
+Get-Mailbox
+
+Get-mailbox "tim.taylor" | select -ExpandProperty emailaddresses
+
+Import-CSV "C:\import\Email-Adressen.csv" | ForEach {Set-Mailbox $_.Mailbox –EmailAddresses @{Add=$_.NewEmailAddress}}
+
+Get-mailbox "tim.taylor" | select -ExpandProperty emailaddresses
+
+Get-mailbox "tim.allen" | select -ExpandProperty emailaddresses
+
+Remove-PSSession $session
