1f5763505676f497534c962d660db9b120c4e41c
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2020-11-01 16:18:38 +0100

    Add files via upload

---
+++ Create_Team_and_Channel.ps1
@@ -0,0 +1,29 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module MicrosoftTeams -AllowClobber -Force -Verbose
+
+#Update
+Update-Module MicrosoftTeams
+
+#Connect to Microsoft Teams
+Connect-MicrosoftTeams
+
+#All Teams
+Get-Team
+
+#Create new team
+New-Team -DisplayName “Patricks Team ist Cool” -Visibility Public
+
+#Use the New-TeamChannel cmdlet to create a new standard channel
+Get-Team -DisplayName "Logistik" | New-TeamChannel -DisplayName "Wartung" -Description "A channel for maintenance."
+
+#To list all channels of a specific team
+Get-Team -DisplayName "Logistik" | Get-TeamChannel
+
+#To modify a channel, use the Set-TeamChannel
+Get-Team -DisplayName "Logistik" | Set-TeamChannel -CurrentDisplayName "Wartung" -NewDisplayName "Maintenance2020"
+
+#Remove a channel, by using the Remove-TeamChannel
+Get-Team -DisplayName "Logistik" | Remove-TeamChannel -DisplayName "Maintenance2020"
+
