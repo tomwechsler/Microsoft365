31e0fdff32d71aaffc4cb343c9a13039bfdc4302
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2020-11-21 15:27:47 +0100

    Update Get_Guest_User_Last_Login.ps1

--- Get_Guest_User_Last_Login.ps1
+++ Get_Guest_User_Last_Login.ps1
@@ -5,35 +5,44 @@ Clear-Host
 Install-Module MSOnline -AllowClobber -Force -Verbose
 
 #Variables 
-$startDate = "{0:yyyy-MM-dd}" -f (get-date).AddDays(-90)	#90 days prior date.
-$endDate = "{0:yyyy-MM-dd}" -f (get-date)	#current date.
+#90 days prior date
+$startDate = "{0:yyyy-MM-dd}" -f (get-date).AddDays(-90)
+#current date
+$endDate = "{0:yyyy-MM-dd}" -f (get-date)
 $externalUserExtention = "*#EXT#*"
 
-#Path for log file.
-$filePath="<filepath>\logs.txt"
+#Path for log file
+$filePath="C:\Temp\logs.txt"
 
 #Get Credentials
 $credentials=Get-Credential
 
-#Load Modules for exchange online
+#Create the session
 $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credentials -Authentication Basic -AllowRedirection
+
+#Import the session
 Import-PSSession $session -DisableNameChecking -AllowClobber
 
-#Log to text file.
+#Log to text file
 function logtoText($filePath, $msg){$msg >> $filepath;}
-#Log to text ends here.
+#Log to text ends here
 
-#Get All External Users
+#Connect to Microsoft 365 (formerly Office365)
 Connect-MsolService -Credential $credentials
+
+#Search for all external users
 $allExternalUsers = Get-MsolUser | Where-Object -FilterScript { $_.UserPrincipalName -Like $externalUserExtention }
 
-#Get Unified Audit Log for all users.
+#Get Unified Audit Log for all users
 ForEach($externalUser in $allExternalUsers)
 {
-#Get the last login date.
+#Get the last login date
 $lastLoginDate =Search-UnifiedAuditLog -UserIds  $externalUser.UserPrincipalName -StartDate $startDate -EndDate $endDate| Foreach-Object {$_.CreationDate = [DateTime]$_.CreationDate; $_} | Group-Object UserIds | Foreach-Object {$_.Group | Sort-Object CreationDate | Select-Object -Last 1} | Select CreationDate
-#Log the details.
+#Log the details
 Write-Host "User -" $externalUser.UserPrincipalName "| Last Login Date -" $lastLoginDate.CreationDate
 logtoText $filePath ("User - " + $externalUser.UserPrincipalName + "| Last Login Date - " + $lastLoginDate.CreationDate)
 }
 #Get All External Users ends here
+
+#Remove the session 
+Remove-PSSession $Session
