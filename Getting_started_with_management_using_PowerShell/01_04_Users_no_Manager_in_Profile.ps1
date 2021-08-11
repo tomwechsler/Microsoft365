847567df24439e6b3e037af44f9bf0f766e15db9
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-08-11 14:26:58 +0200

    Add files via upload

---
+++ Getting_started_with_management_using_PowerShell/01_04_Users_no_Manager_in_Profile.ps1
@@ -0,0 +1,21 @@
+﻿Set-Location c:\
+Clear-Host
+
+#Which users have no manager?
+$Users = Get-AzureADUser -All $True | Where {$_.UserType -eq 'Member' -and $_.AssignedLicenses -ne $null}
+
+#Create a empty array
+$NoManagerUsers = @()
+
+#Loop to search
+foreach ($user in $Users) 
+{
+    $Manager = Get-AzureADUserManager -ObjectId $user.UserPrincipalName
+    if ($Manager -eq $null)
+    {
+        $NoManagerUsers += $user
+    }
+}
+
+#Export to a Csv File
+$NoManagerUsers | Select DisplayName, UserPrincipalName | Export-Csv -Path "C:\Temp\UsersWithNoManager.csv" -NoTypeInformation
