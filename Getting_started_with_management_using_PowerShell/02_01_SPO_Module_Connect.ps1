72929140db4564821487c391a6cb76196e5beb8f
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-08-13 07:12:42 +0200

    Add files via upload

---
+++ Getting_started_with_management_using_PowerShell/02_01_SPO_Module_Connect.ps1
@@ -0,0 +1,19 @@
+﻿Set-Location C:\
+Clear-Host
+
+#The SharePoint Module
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Force -Verbose
+
+#Credential
+$cred = Get-Credential
+
+#Let's connect
+Connect-SPOService `
+    -Url https://tomscloud-admin.sharepoint.com `
+    -Credential $cred
+
+#Did it work?
+Get-SPOSite
+
+#Disconnect
+Disconnect-SPOService
