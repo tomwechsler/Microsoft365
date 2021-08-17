c12a2ce76bc2aa0555f43ac07b1f5f17683d2af1
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-08-17 06:46:00 +0200

    Add files via upload

---
+++ Getting_started_with_management_using_PowerShell/03_01_EXO_Module_Conncect.ps1
@@ -0,0 +1,14 @@
+﻿Set-Location C:\
+Clear-Host
+
+#We need the exchange online modil
+Install-Module -Name ExchangeOnlineManagement -Verbose -AllowClobber -Force
+
+#Create a variable with credentials
+$cred = Get-Credential
+
+#Lets connect
+Connect-ExchangeOnline –Credential $cred
+
+#Did it work?
+Get-Mailbox
