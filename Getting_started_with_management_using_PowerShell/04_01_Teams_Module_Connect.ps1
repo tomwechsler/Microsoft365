f4ac9def15c3517f552c9ac91ca750797f3c4a94
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-08-21 07:15:02 +0200

    Add files via upload

---
+++ Getting_started_with_management_using_PowerShell/04_01_Teams_Module_Connect.ps1
@@ -0,0 +1,14 @@
+﻿Set-Location C:\
+Clear-Host
+
+#To install the teams module
+Install-Module -Name MicrosoftTeams -AllowClobber -Verbose -Force
+
+#Credentials
+$cred = Get-Credential
+
+#Connect to Teams
+Connect-MicrosoftTeams –Credential $cred
+
+#Did it work?
+Get-Team
