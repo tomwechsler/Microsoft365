7c3a9ee6178d52d7d37ba9b21aab35abc59aabd3
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-08-23 06:41:56 +0200

    Add files via upload

---
+++ Getting_started_with_management_using_PowerShell/04_02_Managing_Teams.ps1
@@ -0,0 +1,48 @@
+﻿#List the teams
+Get-Team
+
+#Create a new Team
+New-Team `
+    -DisplayName "Accounting Team" `
+    -Description "Internal Collaboration place for the Accounting Team" `
+    -Visibility Private
+
+#Some team settings
+Set-Team -GroupId 6226df7e-9fa8-4814-8d20-7a9938b27200 `
+	-Visibility Public `
+	-AllowChannelMentions $false `
+	-AllowCreateUpdateChannels $false `
+    -AllowUserDeleteMessages $false `
+    -AllowUserEditMessages $false `
+    -AllowGiphy $false `
+	-AllowStickersAndMemes $false
+
+#Add a channel
+New-TeamChannel `
+    -GroupId 6226df7e-9fa8-4814-8d20-7a9938b27200 `
+	-DisplayName "Milestones” `
+    -MembershipType Private
+
+#Some channel settings
+Set-TeamChannel -GroupId 6226df7e-9fa8-4814-8d20-7a9938b27200 `
+	-CurrentDisplayName "Milestones" `
+	-NewDisplayName "Targets" `
+	-Description "Use this channel to share the project targets"
+
+#List owner
+Get-TeamUser -GroupId  6226df7e-9fa8-4814-8d20-7a9938b27200 -Role Owner
+
+#List member
+Get-TeamUser -GroupId  6226df7e-9fa8-4814-8d20-7a9938b27200 -Role Member
+
+#Add a member
+Add-TeamUser -GroupId 6226df7e-9fa8-4814-8d20-7a9938b27200 `
+    -User fred.jonas@tomrocks.ch
+
+#Add an owner
+Add-TeamUser -GroupId 6226df7e-9fa8-4814-8d20-7a9938b27200 `
+    -User fred.jonas@tomrocks.ch `
+    -Role Owner
+
+#Remove a team
+Remove-Team -GroupId 6226df7e-9fa8-4814-8d20-7a9938b27200
---
+++ Getting_started_with_management_using_PowerShell/04_03_Teams_Policy_Investigate.ps1
@@ -0,0 +1,65 @@
+﻿Set-Location c:\
+Clear-Host
+
+#Install the teams module
+Install-Module MicrosoftTeams -AllowClobber -Force -Verbose
+
+#Install the MSOnline module
+Install-Module MSOnline -AllowClobber -Force -Verbose
+
+#Import the teams module
+Import-Module -Name MicrosoftTeams
+
+#Credentials
+$cred = Get-Credential
+
+#Connect to Teams
+Connect-MicrosoftTeams –Credential $cred
+
+#Connect to Microsoft 365 (formerly Office 365)
+Connect-MsolService –Credential $cred
+
+#Did it work
+Get-MsolUser
+
+#Did it work
+Get-Team
+
+#We need the ObjectId of a user
+Get-MsolUser -UserPrincipalName "tom@tomscloud.ch" | Select-Object ObjectId
+
+#Check the assigment for a user
+Get-CsUserPolicyAssignment -Identity ba36d788-31a5-4f72-9709-7d586056762c
+
+#Check all users for some policies
+Get-CsOnlineUser | Format-Table UserPrincipalName, TeamsMessagingPolicy, TeamsMeetingPolicy, TeamsAppSetupPolicy
+
+#The details of the policy
+Get-CsTeamsMessagingPolicy -Identity "Tom"
+
+#To grant a single user a Messaging Policy
+Grant-CsTeamsMessagingPolicy -Identity fred.jonas@tomscloud.ch -PolicyName "Tom"
+
+#Search all users in the department "Administration"
+Get-CsOnlineUser -Filter {Department -eq 'Administration'} | Select UserPrincipalName
+
+#Set a Policy for the specific users
+Get-CsOnlineUser -Filter {Department -eq 'Administration'} | Grant-CsTeamsMessagingPolicy -PolicyName "Tom"
+
+#Let's check
+Get-CsOnlineUser -Filter {TeamsMessagingPolicy -eq 'Tom'} | Select UserPrincipalName
+
+#an other way
+Get-CsOnlineUser | Format-Table UserPrincipalName, TeamsMessagingPolicy, TeamsMeetingPolicy, TeamsAppSetupPolicy
+
+#Undo
+Get-CsOnlineUser -Filter {Department -eq 'Administration'} | Grant-CsTeamsMessagingPolicy -PolicyName $Null
+
+#Other example
+Get-CsOnlineUser -Filter {Department -eq 'Administration'} | Grant-CsTeamsMeetingPolicy -PolicyName $Null
+
+#Other example
+Get-CsOnlineUser -Filter {Department -eq 'Administration'} | Grant-CsTeamsAppSetupPolicy -PolicyName $Null
+
+#Remove a policy from a user
+Grant-CsTeamsMessagingPolicy -Identity fred.jonas@tomscloud.ch -PolicyName $null
