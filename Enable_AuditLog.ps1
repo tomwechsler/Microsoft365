37bf4b31b72938116b4f88281d4829a706eb03af
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-09-01 07:35:56 +0200

    Add files via upload

---
+++ Enable_AuditLog.ps1
@@ -0,0 +1,26 @@
+﻿Set-Location C:\
+Clear-Host
+
+#We need the exchange online modil
+Install-Module -Name ExchangeOnlineManagement -Verbose -AllowClobber -Force
+
+#Create a variable with credentials
+$cred = get-credential
+
+#Lets connect
+Connect-ExchangeOnline –Credential $cred
+
+#Did it work?
+Get-Mailbox
+
+#Verify the auditing status for your organization
+Get-AdminAuditLogConfig | FL UnifiedAuditLogIngestionEnabled
+
+#Use PowerShell to turn on auditing
+Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
+
+#Turn off auditing
+Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $false
+
+#Verify the auditing status for your organization
+Get-AdminAuditLogConfig | FL UnifiedAuditLogIngestionEnabled
