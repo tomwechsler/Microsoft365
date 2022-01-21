0a60c0dd242c4447660c5ede85c0ead1ffd067b7
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-01-21 05:33:06 +0100

    Add files via upload

---
+++ Dehydrated_Microsoft365_Tenant.ps1
@@ -0,0 +1,20 @@
+﻿Set-Location C:\
+Clear-Host
+
+#We need the exchange online modil
+Install-Module -Name ExchangeOnlineManagement -Verbose -AllowClobber -Force
+
+#Lets connect
+Connect-ExchangeOnline
+
+#Did it work?
+Get-Mailbox
+
+#We can check the dehydrate status using the below command
+Get-OrganizationConfig | Format-List Identity,IsDehydrated
+
+#If the Microsoft 365 tenant is dehydrated. We can hydrate the tenant using the below command
+Enable-OrganizationCustomization
+
+#We can check the dehydrate status using the below command
+Get-OrganizationConfig | Format-List Identity,IsDehydrated
